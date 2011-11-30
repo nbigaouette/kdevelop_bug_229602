8c7333c08f9e125a9a9a30aabfa96519e9f076f6
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-11-30 12:13:23 -0500

    Deleted files

--- src/Git_Diff.cpp
+++
@@ -1,3 +0,0 @@
-#include "Git_Diff.hpp"
-const unsigned char src_Git_Diff_patch[] = "";
-const unsigned int src_Git_Diff_patch_len = 0;
--- src/Version.cpp
+++
@@ -1,5 +0,0 @@
-#include "Version.hpp"
-const char *git_build_sha = "HEAD";
-const char *git_build_branch = "";
-const char *git_build_time = "Wed Nov 30 11:51:36 EST 2011";
-const char *git_log_stat = "";
