a78724aa5b1a82cb02059badfa066338c70d7183
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-11-30 12:15:55 -0500

    Cleanup: deleted hello world stuff.

--- src/Main.cpp
+++ src/Main.cpp
@@ -31,21 +31,6 @@ inline std::string IntToStr(const Integer integer, const int width = 0, const ch
 // **************************************************************
 int main(int argc, char *argv[])
 {
-    std::cout << "Hello world!" << std::endl;
-    std::cout << "sizeof(int) = " << sizeof(int) << std::endl;
-    std::cout << "sizeof(long int) = " << sizeof(long int) << std::endl;
-    std::cout << "sizeof(long long int) = " << sizeof(long long int) << std::endl;
-    std::cout << "sizeof(unsigned int) = " << sizeof(unsigned int) << std::endl;
-    std::cout << "sizeof(unsigned long int) = " << sizeof(unsigned long int) << std::endl;
-    std::cout << "sizeof(unsigned long long int) = " << sizeof(unsigned long long int) << std::endl;
-    std::cout << "sizeof(uint64_t) = " << sizeof(uint64_t) << std::endl;
-    std::cout << "sizeof(uint32_t) = " << sizeof(uint32_t) << std::endl;
-    std::cout << "sizeof(int64_t) = " << sizeof(int64_t) << std::endl;
-    std::cout << "sizeof(int32_t) = " << sizeof(int32_t) << std::endl;
-    std::cout << "sizeof(std::vector<uint64_t>::size_type) = " << sizeof(std::vector<uint64_t>::size_type) << std::endl;
-    std::cout << "sizeof(std::vector<double>::size_type) = " << sizeof(std::vector<double>::size_type) << std::endl;
-    std::cout << "sizeof(std::vector<float>::size_type) = " << sizeof(std::vector<float>::size_type) << std::endl;
-
     Log_Git_Info(std::string("output/"));
 
 
