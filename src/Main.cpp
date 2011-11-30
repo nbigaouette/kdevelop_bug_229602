1794d5fc0dd9909b490a4f9881c90011f1624ce5
Nicolas Bigaouette <nbigaouette@gmail.com>
2011-11-30 12:10:07 -0500

    Create 10,000 binary files in output/

--- src/Main.cpp
+++ src/Main.cpp
@@ -7,9 +7,26 @@
 #include <iostream>
 #include <vector>
 #include <stdint.h> // uint64_t
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cassert>
 
 #include "Version.hpp"
 #include "Git_Diff.hpp"
+
+template <class Integer>
+inline std::string IntToStr(const Integer integer, const int width = 0, const char fill = ' ')
+{
+    std::ostringstream MyStream;
+    if (width != 0)
+    {
+        MyStream << std::setw(width);
+        MyStream << std::setfill(fill);
+    }
+    MyStream << integer << std::flush;
+    return (MyStream.str());
+}
 
 // **************************************************************
 int main(int argc, char *argv[])
@@ -30,6 +47,24 @@ int main(int argc, char *argv[])
     std::cout << "sizeof(std::vector<float>::size_type) = " << sizeof(std::vector<float>::size_type) << std::endl;
 
     Log_Git_Info(std::string("output/"));
+
+
+    // Allocate some data
+    double *ptr = new double[100];
+
+    // Save it in the output folder
+    std::ofstream fp_out;
+    std::string filename;
+    for (int i = 0 ; i < 10000 ; i++)
+    {
+        filename = std::string("output/data_") + IntToStr<int>(i, 6, 0) + ".bin";
+        fp_out.open(filename.c_str(), std::ios::out | std::ios::binary);
+        assert(fp_out.is_open());
+        fp_out.write(ptr, 100*sizeof(double));
+        fp_out.close();
+    }
+
+    delete[] ptr;
 
     return EXIT_SUCCESS;
 }
